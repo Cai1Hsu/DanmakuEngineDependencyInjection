508650e4d9fc9746d8220141d889580623307513
Caiyi Shyu <cai1hsu@outlook.com>
2024-10-07 21:21:28 +0800

    add tests for container class modifier analyzing

---
+++ DanmakuEngine.DependencyInjection.SourceGeneration.Tests/ContainerModifierAnalyzerTests.cs
@@ -0,0 +1,90 @@
+using Verifier = DanmakuEngine.DependencyInjection.SourceGeneration.Tests.Verifiers.CSharpAnalyzerVerifier<DanmakuEngine.DependencyInjection.SourceGeneration.Analyzers.ContainerClassAnalyzer>;
+
+namespace DanmakuEngine.DependencyInjection.SourceGeneration.Tests;
+
+public class ContainerModifierAnalyzerTests
+{
+    [Test]
+    public async Task Flag_WhenNoPartialModifier()
+    {
+        const string text = """
+                            using DanmakuEngine.DependencyInjection;
+                            
+                            [DependencyContainer]
+                            class {|#0:MyContainer|}
+                            {
+                            }
+                            """;
+
+        var expected = Verifier.Diagnostic(ContainerModifierRule.ContainerMustBePartial).WithLocation(0);
+
+        await Verifier.VerifyAnalyzerAsync(text, expected).ConfigureAwait(false);
+    }
+
+    [Test]
+    public async Task Flag_WhenContainingClassIsNotPartial()
+    {
+        const string text = """
+                            using DanmakuEngine.DependencyInjection;
+                            
+                            class {|#0:Foo|}
+                            {
+                                [DependencyContainer]
+                                partial class MyContainer
+                                {
+                                }
+                            }
+                            """;
+
+        var expected = Verifier.Diagnostic(ContainerModifierRule.ContainerMustBePartial).WithLocation(0);
+
+        await Verifier.VerifyAnalyzerAsync(text, expected).ConfigureAwait(false);
+    }
+
+    [Test]
+    public async Task No_Error_WhenPartialModifier()
+    {
+        const string text = """
+                            using DanmakuEngine.DependencyInjection;
+                            
+                            [DependencyContainer]
+                            partial class MyContainer
+                            {
+                            }
+                            """;
+
+        await Verifier.VerifyAnalyzerAsync(text).ConfigureAwait(false);
+    }
+
+    [Test]
+    public async Task Flag_WhenStaticModifierAdded()
+    {
+        const string text = """
+                            using DanmakuEngine.DependencyInjection;
+                            
+                            [DependencyContainer]
+                            {|#0:static|} partial class MyContainer
+                            {
+                            }
+                            """;
+
+        var expected = Verifier.Diagnostic(ContainerModifierRule.ContainerCanNotBeStatic).WithLocation(0);
+
+        await Verifier.VerifyAnalyzerAsync(text, expected).ConfigureAwait(false);
+    }
+
+    [Test]
+    public async Task No_Error_WhenStaticModifierNotAdded()
+    {
+        const string text = """
+                            using DanmakuEngine.DependencyInjection;
+                            
+                            [DependencyContainer]
+                            partial class MyContainer
+                            {
+                            }
+                            """;
+
+        await Verifier.VerifyAnalyzerAsync(text).ConfigureAwait(false);
+    }
+}
