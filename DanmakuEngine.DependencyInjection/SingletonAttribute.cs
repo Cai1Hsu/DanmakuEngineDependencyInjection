b1589b63596565602f9bc375ddb635486e5db130
Caiyi Shyu <cai1hsu@outlook.com>
2024-10-07 22:13:21 +0800

    constraint TImplementation inherits TDependency

--- DanmakuEngine.DependencyInjection/SingletonAttribute.cs
+++ DanmakuEngine.DependencyInjection/SingletonAttribute.cs
@@ -7,5 +7,6 @@ public class SingletonAttribute<TDependency> : Attribute
 
 [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
 public class SingletonAttribute<TDependency, TImplementation> : SingletonAttribute<TDependency>
+    where TImplementation : TDependency
 {
 }
