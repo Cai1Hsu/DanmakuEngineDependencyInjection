8b8adbfbdaceb0755a819881a72a5834ad4b8ecc
Caiyi Shyu <cai1hsu@outlook.com>
2024-10-07 22:12:30 +0800

    allow multiple SingletonAttribute usage

--- DanmakuEngine.DependencyInjection/SingletonAttribute.cs
+++ DanmakuEngine.DependencyInjection/SingletonAttribute.cs
@@ -1,11 +1,11 @@
 namespace DanmakuEngine.DependencyInjection;
 
-[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
 public class SingletonAttribute<TDependency> : Attribute
 {
 }
 
-[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
 public class SingletonAttribute<TDependency, TImplementation> : SingletonAttribute<TDependency>
 {
 }
