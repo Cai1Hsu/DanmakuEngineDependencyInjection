850a847e8d4e0ed57adf9f2ef7a01613006329b7
Caiyi Shyu <cai1hsu@outlook.com>
2024-10-09 18:19:34 +0800

    Update `ConstructorAnalysisRule` to match `IContainerClassAnalysisRule`

--- DanmakuEngine.DependencyInjection.SourceGeneration/Analyzers/ConstructorAnalysisRule.cs
+++ DanmakuEngine.DependencyInjection.SourceGeneration/Analyzers/ConstructorAnalysisRule.cs
@@ -6,21 +6,25 @@ using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace DanmakuEngine.DependencyInjection.SourceGeneration.Analyzers;
 
-public class ConstructorRule : IContainerClassAnalysisRule
+public class ConstructorAnalysisRule : IContainerClassAnalysisRule
 {
     public ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => throw new NotImplementedException();
 
     public bool RequiredToBeContainer => true;
 
+    public bool WantMarkerRegistrationType => true;
+
+    public bool ValidOnTypesWithoutRegistrations => false;
+
     public const string ConstructorMethod = ".ctor";
 
-    public void AnalyzeSymbol(SymbolAnalysisContext context, bool isContainer)
+    public void AnalyzeSymbol(SymbolAnalysisContext context, bool isContainer, bool hasRegistrations)
     {
         INamedTypeSymbol containerTypeSymbol = (INamedTypeSymbol)context.Symbol;
 
         var containerAttributes = containerTypeSymbol.GetAttributes()
             .Where(a => a.AttributeClass is not null)
-            .Where(a => DependencyRegistrationRule.RegistrationAttributes.Contains(a.AttributeClass!.ToGlobalPrefixedFullName()));
+            .Where(a => ContainerClassAnalyzer.RegistrationAttributes.Contains(a.AttributeClass!.ToGlobalPrefixedFullName()));
 
         var implTypes = containerAttributes.Where(a => a.AttributeClass!.TypeArguments.Length is 1 or 2)
             .Select(a => a.AttributeClass!.TypeArguments)
